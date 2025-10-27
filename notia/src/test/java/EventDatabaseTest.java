b2d21b45941bcf64d52d847e4610b6044b5f9d27
eguitar <eguitarrocks@yahoo.com>
2025-10-27 13:54:13 -0700

    cover event unit tests for talha

---
+++ notia/src/test/java/EventDatabaseTest.java
@@ -0,0 +1,56 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EventDatabaseTest {
+
+    @Test
+    public void testEventDatabaseValidInitialization() {}
+
+    @Test
+    public void testEventDatabaseInvalidInitialization() {}
+
+    @Test
+    public void testAddEventValid() {}
+
+    @Test
+    public void testAddEventDuplicate() {}
+
+    @Test
+    public void testAddEventInvalid() {}
+
+    @Test
+    public void testDeleteEventValidId() {}
+
+    @Test
+    public void testDeleteEventInvalidId() {}
+
+    @Test
+    public void testGetAllEventsValid() {}
+
+    @Test
+    public void testGetAllEventsEmpty() {}
+
+    @Test
+    public void testGetEventByIdValid() {}
+
+    @Test
+    public void testGetEventByIdInvalid() {}
+
+    @Test
+    public void testUpdateEventValidId() {}
+
+    @Test
+    public void testUpdateEventInvalidId() {}
+
+    @Test
+    public void testUpdateEventWithNullData() {}
+
+    @Test
+    public void testUpdateEventOwnership() {}
+
+    @Test
+    public void testEventIntegrationWithUser() {}
+
+    @Test
+    public void testNullEventListException() {}
+}
---
+++ notia/src/test/java/EventTest.java
@@ -0,0 +1,53 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EventTest {
+
+    @Test
+    public void testEventValidInitialization() {}
+
+    @Test
+    public void testEventInvalidInitialization() {}
+
+    @Test
+    public void testSetTitleValid() {}
+
+    @Test
+    public void testSetTitleInvalid() {}
+
+    @Test
+    public void testSetDateValid() {}
+
+    @Test
+    public void testSetDateInvalid() {}
+
+    @Test
+    public void testSetTagsValid() {}
+
+    @Test
+    public void testSetTagsInvalid() {}
+
+    @Test
+    public void testGetTitle() {}
+
+    @Test
+    public void testGetDate() {}
+
+    @Test
+    public void testGetTags() {}
+
+    @Test
+    public void testEditEventWithValidData() {}
+
+    @Test
+    public void testEditEventWithInvalidData() {}
+
+    @Test
+    public void testNullTitleException() {}
+
+    @Test
+    public void testNullDateException() {}
+
+    @Test
+    public void testEventOwnershipInEventDatabase() {}
+}
