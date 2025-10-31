1cb235d8b67e9df37cc8d476c7b4686c4e90fc8a
eguitar <eguitarrocks@yahoo.com>
2025-10-30 23:30:32 -0700

    implementation update some user class

--- notia/src/main/java/cs580/UserData.java
+++ notia/src/main/java/cs580/UserData.java
@@ -1,5 +1,44 @@
 package cs580;
 
 public class UserData {
-    
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String email;
+
+
+
+
+
+    public UserData(String firstName, String lastName, String username,
+                                        String password, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+
+
+    public void updateUserName() {}
+
+    public void updatePassword() {}
+
+    public void updateEmail() {}
+
+
+
+    public String getUserDataSummary() { return;}
+
+
+
+
+
+
+
+
+
+
 }
