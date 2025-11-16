750c65c8bcab18dc667202c2fa2775265943ab8f
talhaahmed884 <talhaahmed884@gmail.com>
2025-11-16 12:07:05 -0800

    1. updated encapsulation

--- notia/src/main/java/cs580/exception_handling/ExceptionHandler.java
+++ notia/src/main/java/cs580/exception_handling/ExceptionHandler.java
@@ -2,6 +2,6 @@ package cs580.exception_handling;
 
 // Exception handler interface using Template Method pattern
 @FunctionalInterface
-public interface ExceptionHandler<E extends Exception> {
+interface ExceptionHandler<E extends Exception> {
     void handle(E exception) throws E;
 }
--- notia/src/main/java/cs580/exception_handling/ValidationStrategy.java
+++ notia/src/main/java/cs580/exception_handling/ValidationStrategy.java
@@ -2,6 +2,6 @@ package cs580.exception_handling;
 
 // Generic validation strategy interface - supports any exception type
 @FunctionalInterface
-public interface ValidationStrategy<E extends Exception> {
+interface ValidationStrategy<E extends Exception> {
     void validate() throws E;
 }
